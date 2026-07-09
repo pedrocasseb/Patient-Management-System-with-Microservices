83cf0bf71dcfe1030d622b09a6b9ff60001a0bb3
Pedro Casseb <casseb.phcc@gmail.com>
2026-07-09 15:09:23 -0300

    feat(auth-service): add user model, docker configuration, and database initialization

---
+++ auth-service/src/main/java/com/pm/authservice/model/User.java
@@ -0,0 +1,26 @@
+package com.pm.authservice.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
