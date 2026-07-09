bac4bee78b213bfdc6ba6ceda7d863b1ef76c3db
Pedro Casseb <casseb.phcc@gmail.com>
2026-07-09 12:59:58 -0300

    feat(analytics-service): initialize service with Kafka and gRPC support

---
+++ analytics-service/src/main/java/com/pm/analyticsservice/kafka/KafkaConsumer.java
@@ -0,0 +1,21 @@
+package com.pm.analyticsservice.kafka;
+
+import com.google.protobuf.InvalidProtocolBufferException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+import patient.events.PatientEvent;
+
+@Slf4j
+@Service
+public class KafkaConsumer {
+    @KafkaListener(topics = "patient", groupId = "analytics-service")
+    public void consumeEvent(byte[] event) {
+        try {
+            PatientEvent patientEvent = PatientEvent.parseFrom(event);
+            log.info("Received patient event: [PatientId={}, PatientName={}, PatientEmail{}]", patientEvent.getPatientId(), patientEvent.getName(), patientEvent.getEmail());
+        } catch (InvalidProtocolBufferException e) {
+            log.error("Error deserializing event {}", e.getMessage());
+        }
+    }
+}
